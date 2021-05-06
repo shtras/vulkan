466cc10c01e0a7fde2c5a57e739a703e1cc646d0
Gregory Shtrasberg <shtras@gmail.com>
2021-05-05 23:02:58 -0400

    Added spdlog. Fixed warnings. Advanced in the tutorial

--- src/Test2.cpp
+++ src/Test2.cpp
@@ -5,6 +5,9 @@
 #include <stdexcept>
 #include <cstdlib>
 
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+
 class HelloTriangleApplication
 {
 public:
@@ -28,6 +31,17 @@ private:
 
     void initVulkan()
     {
+        createInstance();
+    }
+
+    void createInstance()
+    {
+        VkApplicationInfo appInfo{.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+            .pApplicationName = "Hello Triangle",
+            .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+            .pEngineName = "No Engine",
+            .engineVersion = VK_MAKE_VERSION(1, 0, 0),
+            .apiVersion = VK_API_VERSION_1_0};
     }
 
     void mainLoop()
@@ -46,10 +60,16 @@ private:
     GLFWwindow* window = nullptr;
     static constexpr uint32_t WIDTH = 800;
     static constexpr uint32_t HEIGHT = 600;
+
+    VkInstance instance = nullptr;
 };
 
 int main()
 {
+    spdlog::set_default_logger(
+        spdlog::stdout_color_mt(std::string("logger"), spdlog::color_mode::always));
+    spdlog::set_level(spdlog::level::debug);
+    spdlog::debug("Hello!");
     HelloTriangleApplication app;
 
     try {
