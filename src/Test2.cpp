d816a8410e808e0490440fc64ce76d2141121c32
Gregory Shtrasberg <shtras@gmail.com>
2021-05-09 18:11:03 -0400

    Crossplatform much

--- src/Test2.cpp
+++ src/Test2.cpp
@@ -1,12 +1,17 @@
 #include "Utils/Utils.h"
 
+#ifdef WIN32
 #pragma warning(push)
 #pragma warning(disable : 26812)
-
+#define GLFW_EXPOSE_NATIVE_WIN32
 #define VK_USE_PLATFORM_WIN32_KHR
+#else
+#define GLFW_EXPOSE_NATIVE_X11
+#define VK_USE_PLATFORM_XLIB_KHR
+#endif
+
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-#define GLFW_EXPOSE_NATIVE_WIN32
 #include <GLFW/glfw3native.h>
 
 #include <iostream>
@@ -1025,4 +1030,6 @@ int main()
 
     return EXIT_SUCCESS;
 }
+#ifdef WIN32
 #pragma warning(pop)
+#endif
